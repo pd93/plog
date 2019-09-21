547cf7f45dda950a37fa24d8067850de27a61991
Pete Davison <pd93.uk@outlook.com>
2019-09-21 01:18:50 +0000

    Added file writer example and simplified the basic example

--- examples/file_example/main.go
+++ examples/file_example/main.go
@@ -7,15 +7,31 @@ import (
 )
 
 func main() {
-	if err := basicExample(); err != nil {
+	if err := fileExample(); err != nil {
 		log.Fatal(err)
 	}
 }
 
-func basicExample() (err error) {
+func fileExample() (err error) {
+
+	// Open a JSON file
+	jsonWriter, err := log.NewFile("log.json")
+	if err != nil {
+		return err
+	}
+	defer jsonWriter.Close()
+
+	// Open a CSV file
+	csvWriter, err := log.NewFile("log.csv")
+	if err != nil {
+		return err
+	}
+	defer csvWriter.Close()
 
 	// Create some loggers
 	log.AddLogger("std", log.NewLogger())
+	log.AddLogger("json", log.NewJSONFileLogger(jsonWriter))
+	log.AddLogger("csv", log.NewCSVFileLogger(csvWriter))
 
 	// Write to all loggers
 	log.Fatal(errors.New("Fatal log"))
@@ -31,6 +47,9 @@ func basicExample() (err error) {
 	// Change some settings
 	stdLogger.SetLogLevel(log.TraceLevel)
 	stdLogger.SetTimestampFormat("Mon Jan 2 15:04:05 -0700 UTC 2006")
+
+	// Write to a specific logger
+	stdLogger.Info("Special log that only prints to stdout")
 
 	// Write to all loggers again
 	log.Fatal(errors.New("Fatal log"))
