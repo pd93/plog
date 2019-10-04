4c7f65efaf2277f6e613281cb794080831db0a5b
Pete Davison <pd93.uk@outlook.com>
2019-10-04 03:47:55 +0000

    Added Tags to the Log structure - Also added two functions: newTLog and newTLogf

--- log.go
+++ log.go
@@ -10,6 +10,7 @@ type log struct {
 	logLevel  LogLevel
 	variables []interface{}
 	timestamp time.Time
+	tags      Tags
 }
 
 // newLog creates a new instance of log and populates it with a log level and a message
@@ -22,9 +23,27 @@ func newLog(logLevel LogLevel, variables ...interface{}) *log {
 	}
 }
 
+// newTLog creates a new instance of log and populates it with a log level, a message and a series of meta-tags
+// A timestamp is also generated and stored
+func newTLog(logLevel LogLevel, tags Tags, variables ...interface{}) *log {
+	return &log{
+		logLevel:  logLevel,
+		variables: variables,
+		timestamp: time.Now(),
+		tags:      tags,
+	}
+}
+
 // newLogf creates a new instance of log and populates it with a log level and a formatted message
 // You can send any number of variables to this function and they will be printed according to the format specified
 // A timestamp is also generated and stored
 func newLogf(level LogLevel, format string, variables ...interface{}) *log {
 	return newLog(level, fmt.Sprintf(format, variables...))
+}
+
+// newTLogf creates a new instance of log and populates it with a log level, a formatted message and a series of meta-tags
+// You can send any number of variables to this function and they will be printed according to the format specified
+// A timestamp is also generated and stored
+func newTLogf(level LogLevel, tags Tags, format string, variables ...interface{}) *log {
+	return newLog(level, tags, fmt.Sprintf(format, variables...))
 }
