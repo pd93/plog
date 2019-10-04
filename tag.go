031a80713c3c0016acd97278f7e396c8ed5eba20
Pete Davison <pd93.uk@outlook.com>
2019-10-04 05:51:16 +0000

    Tag format methods now color the tag if colorLogging is enabled and a map entry is found

--- tag.go
+++ tag.go
@@ -13,36 +13,54 @@ type Tag string
 type Tags []Tag
 
 // Text will stringify the tags into a readable format
-func (tags Tags) Text() string {
+func (tags Tags) Text(colorLogging bool, tagColorMap TagColorMap) string {
 
 	strSlice := make([]string, len(tags))
 
 	for i, tag := range tags {
-		strSlice[i] = fmt.Sprintf("#%s", tag)
+		if attributes, ok := tagColorMap[tag]; colorLogging && ok {
+			strSlice[i] = color(fmt.Sprintf("#%s", tag), attributes...)
+		} else if colorLogging {
+			strSlice[i] = color(fmt.Sprintf("#%s", tag), FgWhite, Faint)
+		} else {
+			strSlice[i] = fmt.Sprintf("#%s", tag)
+		}
 	}
 
 	return strings.Join(strSlice, " ")
 }
 
 // JSON will stringify the tags into a JSON array
-func (tags Tags) JSON() string {
+func (tags Tags) JSON(colorLogging bool, tagColorMap TagColorMap) string {
 
 	strSlice := make([]string, len(tags))
 
 	for i, tag := range tags {
-		strSlice[i] = strconv.Quote(string(tag))
+		if attributes, ok := tagColorMap[tag]; colorLogging && ok {
+			strSlice[i] = color(strconv.Quote(string(tag)), attributes...)
+		} else if colorLogging {
+			strSlice[i] = color(strconv.Quote(string(tag)), FgWhite, Faint)
+		} else {
+			strSlice[i] = strconv.Quote(string(tag))
+		}
 	}
 
 	return fmt.Sprintf(`[ %s ]`, strings.Join(strSlice, ", "))
 }
 
 // CSV will stringify the tags into a CSV compatible string
-func (tags Tags) CSV() string {
+func (tags Tags) CSV(colorLogging bool, tagColorMap TagColorMap) string {
 
 	strSlice := make([]string, len(tags))
 
 	for i, tag := range tags {
-		strSlice[i] = string(tag)
+		if attributes, ok := tagColorMap[tag]; colorLogging && ok {
+			strSlice[i] = color(string(tag), attributes...)
+		} else if colorLogging {
+			strSlice[i] = color(string(tag), FgWhite, Faint)
+		} else {
+			strSlice[i] = string(tag)
+		}
 	}
 
 	return strings.Join(strSlice, ":")
