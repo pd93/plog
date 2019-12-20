7b4377bb04d60c3e0fd022a7883df2a4e342b832
Pete Davison <pd93.uk@outlook.com>
2019-12-20 19:42:59 +0000

    Added sequencer function and a few built-in sequencers

---
+++ sequencer_datetime.go
@@ -0,0 +1,12 @@
+package plog
+
+import (
+	"fmt"
+	"time"
+)
+
+// DateTimeSequencer will set the name of the file to the current date/time
+// NOTE: We're using microseconds here to make sure that file names don't conflict if they're generated too quickly
+func DateTimeSequencer(format, prev string) (next string, err error) {
+	return fmt.Sprintf(format, time.Now().UTC().Format("2006-01-02T15:04:05.000Z07:00")), nil
+}
