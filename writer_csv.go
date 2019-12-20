c748d17071d33065c6ed365b0a475c264746750c
Pete Davison <pd93.uk@outlook.com>
2019-12-20 19:39:21 +0000

    Converted writers to functions

--- writer_csv.go
+++ writer_csv.go
@@ -5,34 +5,17 @@ import (
 	"os"
 )
 
-// CSVFileWriter will write lines of CSV to a file and formats it
-type CSVFileWriter struct {
-	file *os.File
-}
-
 // String constants
 const csvHeader = "Timestamp,LogLevel,Message,Tags\n"
 
-// NewCSVFileWriter will create and return a new CSV file writer
-func NewCSVFileWriter(filePath string) (*CSVFileWriter, error) {
-
-	// Open the file for appending
-	file, err := os.OpenFile(filePath, os.O_APPEND|os.O_CREATE|os.O_RDWR, 0644)
-	if err != nil {
-		return nil, err
-	}
-
-	return &CSVFileWriter{file: file}, nil
-}
-
 // Write will write bytes to the file
 // TODO: It might be nice to automatically remove extra whitespace from the file
-func (writer *CSVFileWriter) Write(p []byte) (n int, err error) {
+func CSVWriter(file *os.File, p []byte) (n int, err error) {
 
 	var m int
 
 	// Get the file size
-	fileInfo, err := writer.file.Stat()
+	fileInfo, err := file.Stat()
 	if err != nil {
 		return
 	}
@@ -40,19 +23,19 @@ func (writer *CSVFileWriter) Write(p []byte) (n int, err error) {
 
 	// If the file is empty, initialise it
 	if size == 0 {
-		n, err = writer.Init()
+		n, err = initCSVFile(file)
 		if err != nil {
 			return
 		}
 	}
 
 	// If the file is invalid
-	if ok, err := writer.IsValid(); !ok {
+	if ok, err := isCSVFileValid(file); !ok {
 		return n, err
 	}
 
 	// Write the log message
-	m, err = writer.file.Write(p)
+	m, err = file.Write(p)
 	if err != nil {
 		return
 	}
@@ -62,40 +45,35 @@ func (writer *CSVFileWriter) Write(p []byte) (n int, err error) {
 }
 
 // Init will reset the contents of the file
-func (writer *CSVFileWriter) Init() (n int, err error) {
+func initCSVFile(file *os.File) (n int, err error) {
 
 	// Delete everything
-	if err = writer.file.Truncate(0); err != nil {
+	if err = file.Truncate(0); err != nil {
 		return
 	}
 
 	// Move the cusor back to zero
-	if _, err = writer.file.Seek(0, 0); err != nil {
+	if _, err = file.Seek(0, 0); err != nil {
 		return
 	}
 
 	// Write the header
-	return writer.file.Write([]byte(csvHeader))
+	return file.Write([]byte(csvHeader))
 }
 
-// IsValid will return whether or not the file contains valid CSV
-func (writer *CSVFileWriter) IsValid() (bool, error) {
+// isJSONFileValid will return whether or not the CSV file is valid
+func isCSVFileValid(file *os.File) (bool, error) {
 
 	// Get the first line of the file
 	firstLine := make([]byte, len(csvHeader))
-	if _, err := writer.file.ReadAt(firstLine, 0); err != nil {
+	if _, err := file.ReadAt(firstLine, 0); err != nil {
 		return false, err
 	}
 
 	// Check if it matches the CSV header
 	if string(firstLine) != csvHeader {
-		return false, fmt.Errorf("File: '%s' is not valid", writer.file.Name())
+		return false, fmt.Errorf("File: '%s' is not valid", file.Name())
 	}
 
 	return true, nil
-}
-
-// Close wraps the file.Close() method
-func (writer *CSVFileWriter) Close() {
-	writer.file.Close()
 }
