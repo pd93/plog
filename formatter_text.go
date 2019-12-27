224d93e1b42a01a3a9a2951f69daa74c49e15d92
Pete Davison <pd93.uk@outlook.com>
2019-12-27 01:48:51 +0000

    The '#' in printed tags are now colored too if the tag text is colored

--- formatter_text.go
+++ formatter_text.go
@@ -2,10 +2,13 @@ package plog
 
 import (
 	"fmt"
+	"regexp"
 	"strings"
 )
 
 // TextFormatter will create a readable string using given log and logger configuration
+// TODO: There is probably a better way to put the tag inside the color formatting
+// TODO: Should the color formatting happen here?
 func TextFormatter(timestamp, logLevel string, variables []interface{}, tags []string) (string, error) {
 
 	strVariables := make([]string, len(variables))
@@ -15,11 +18,23 @@ func TextFormatter(timestamp, logLevel string, variables []interface{}, tags []s
 		strVariables[i] = fmt.Sprintf("%v", variable)
 	}
 
+	// Create a regular expression to check for color in the tags
+	re := regexp.MustCompile("^(\\x1b\\[\\d{1,2}(?:;\\d{1,2})*m)(.*)(\\x1b\\[0m)$")
+
 	// Loop through the tags and format them
-	// TODO: Check for color and insert '#' inside color boundary
-	for _, tag := range tags {
-		tag = fmt.Sprintf("#%s", tag)
+	for i := range tags {
+
+		// Find out if the tag has color formatting
+		matches := re.FindAllStringSubmatch(tags[i], -1)
+
+		if len(matches) > 0 {
+			// Add a '#' INSIDE the color formatting string
+			tags[i] = fmt.Sprintf("%s#%s%s", matches[0][1], matches[0][2], matches[0][3])
+		} else {
+			// Add a '#'
+			tags[i] = fmt.Sprintf("#%s", tags[i])
+		}
 	}
 
-	return strings.TrimSpace(fmt.Sprintf("%s [%s] %s %s", timestamp, logLevel, strings.Join(strVariables, " "), tags)), nil
+	return strings.TrimSpace(fmt.Sprintf("%s [%s] %s %s", timestamp, logLevel, strings.Join(strVariables, " "), strings.Join(tags, " "))), nil
 }
