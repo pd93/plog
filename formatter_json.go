f37c5660337d6e82ad8de6823b58ab86834dc60d
Pete Davison <pd93.uk@outlook.com>
2019-12-27 01:49:17 +0000

    Use the JSON marshaller in JSONFormatter

--- formatter_json.go
+++ formatter_json.go
@@ -1,51 +1,27 @@
 package plog
 
 import (
-	"fmt"
-	"strconv"
-	"strings"
+	"encoding/json"
 )
 
 // JSONFormatter will create a JSON string using given log and logger configuration
 func JSONFormatter(timestamp, logLevel string, variables []interface{}, tags []string) (string, error) {
 
-	var fields []string
-
-	// Timestamp
-	if timestamp != "" {
-		fields = append(fields, fmt.Sprintf(`"timestamp": "%s"`, timestamp))
+	// Encode the log parameters as a JSON string
+	b, err := json.Marshal(struct {
+		Timestamp string        `json:"timestamp"`
+		LogLevel  string        `json:"logLevel"`
+		Variables []interface{} `json:"variables,omitempty"`
+		Tags      []string      `json:"tags,omitempty"`
+	}{
+		timestamp,
+		logLevel,
+		variables,
+		tags,
+	})
+	if err != nil {
+		return "", err
 	}
 
-	// Log level
-	if logLevel != "" {
-		fields = append(fields, fmt.Sprintf(`"logLevel": %s`, strconv.Quote(logLevel)))
-	}
-
-	// Message
-	if len(variables) > 0 {
-
-		strVariables := make([]string, len(variables))
-
-		// Loop through the variables and format them
-		for i, variable := range variables {
-			// TODO: We should create a JSON object rather than quoting it as a string
-			strVariables[i] = strconv.Quote(fmt.Sprintf("%v", variable))
-		}
-
-		fields = append(fields, fmt.Sprintf(`"message": [ %s ]`, strings.Join(strVariables, ", ")))
-	}
-
-	// Tags
-	if len(tags) > 0 {
-
-		// Loop through the tags and quote them
-		for _, tag := range tags {
-			tag = strconv.Quote(tag)
-		}
-
-		fields = append(fields, fmt.Sprintf(`"tags": [ %s ]`, strings.Join(tags, ", ")))
-	}
-
-	// Set the output
-	return fmt.Sprintf("{ %s }", strings.Join(fields, ", ")), nil
+	return string(b), nil
 }
