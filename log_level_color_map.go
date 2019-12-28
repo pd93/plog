9fcb87a1e2ec23a5e7e070a9664c24cade0e1763
Pete Davison <pd93.uk@outlook.com>
2019-12-28 01:19:54 +0000

    Added functional arguments to the tag and log level color maps

--- log_level_color_map.go
+++ log_level_color_map.go
@@ -3,19 +3,42 @@ package plog
 // LogLevelColorMap lists which text attributes should be used for each log level
 type LogLevelColorMap map[LogLevel][]Attribute
 
+// A LogLevelColorMapping is a function that sets a color mapping on a given log level color map
+type LogLevelColorMapping func(logLevelColorMap LogLevelColorMap)
+
 //
 // Constructors
 //
 
 // NewLogLevelColorMap creates and returns an instance of LogLevelColorMap with the default values
-func NewLogLevelColorMap() LogLevelColorMap {
-	return LogLevelColorMap{
+func NewLogLevelColorMap(opts ...LogLevelColorMapping) (logLevelColorMap LogLevelColorMap) {
+
+	// Create a default log level color map
+	logLevelColorMap = LogLevelColorMap{
 		FatalLevel: []Attribute{FgBlack, BgRed},
 		ErrorLevel: []Attribute{FgRed},
 		WarnLevel:  []Attribute{FgYellow},
 		InfoLevel:  []Attribute{FgGreen},
 		DebugLevel: []Attribute{FgCyan},
 		TraceLevel: []Attribute{FgBlue},
+	}
+
+	// Loop through each option and call the functional option
+	for _, opt := range opts {
+		opt(logLevelColorMap)
+	}
+
+	return
+}
+
+//
+// Functional Options
+//
+
+// WithLogLevelColorMapping will return a function that sets a color mapping in a log level color map
+func WithLogLevelColorMapping(logLevel LogLevel, attributes ...Attribute) LogLevelColorMapping {
+	return func(logLevelColorMap LogLevelColorMap) {
+		logLevelColorMap.Set(logLevel, attributes...)
 	}
 }
 
--- tag_color_map.go
+++ tag_color_map.go
@@ -3,13 +3,36 @@ package plog
 // TagColorMap lists which text attributes should be used for each tag
 type TagColorMap map[Tag][]Attribute
 
+// A TagColorMapping is a function that sets a color mapping on a given tag color map
+type TagColorMapping func(tagColorMap TagColorMap)
+
 //
 // Constructors
 //
 
 // NewTagColorMap creates and returns an instance of TagColorMap with the default values
-func NewTagColorMap() TagColorMap {
-	return TagColorMap{}
+func NewTagColorMap(opts ...TagColorMapping) (tagColorMap TagColorMap) {
+
+	// Create a default tag color map
+	tagColorMap = TagColorMap{}
+
+	// Loop through each option and call the functional option
+	for _, opt := range opts {
+		opt(tagColorMap)
+	}
+
+	return
+}
+
+//
+// Functional Options
+//
+
+// WithTagColorMapping will return a function that sets a color mapping in a tag color map
+func WithTagColorMapping(tag Tag, attributes ...Attribute) TagColorMapping {
+	return func(tagColorMap TagColorMap) {
+		tagColorMap.Set(tag, attributes...)
+	}
 }
 
 //
